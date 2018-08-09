1a1cfd816c70d861eef6ba96311a0d038ee4f4b6
Nick Bansal <extreme_wired@msn.com>
2018-08-09 20:21:46 +0100

    Final Commit again

--- src/ValidationComponent.js
+++ src/ValidationComponent.js
@@ -7,9 +7,9 @@ class ValidationComponent extends Component {
 		var value = null
 
 		if (this.props.textLength < 5) {
-			value = 'This passwords is too short'
+			value = 'This word is too short'
 		} else {
-			value = 'This passwords\' length is fine'
+			value = 'This words\' length is fine'
 		}
 
 		return (
